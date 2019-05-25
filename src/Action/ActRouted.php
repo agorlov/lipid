201d4bb128f35f6d660186440cedf27e057931bc
Alexandr Gorlov <a.gorlov@gmail.com>
2019-05-25 13:34:31 +0300

    ActRouted created, ApplicationStd simplified closes #5

---
+++ src/Action/ActRouted.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace AG\WebApp\Action;
+
+use AG\WebApp\Action;
+use AG\WebApp\Response;
+use AG\WebApp\Request;
+use AG\WebApp\NotFoundException;
+use Exception;
+
+/**
+ * Routed action
+ *
+ * Selects the appropriate action for a given link
+ *
+ * @author agorlov
+ */
+final class ActRouted implements Action
+{
+    private $request;
+    private $actionMap;
+
+    /**
+     * Constructor.
+     *
+     * @param string|Request $path - url path (examples: '/' '/page' '/api/list')
+     * @param array $actionMap list of application actions
+     *     [
+     *          '/' =>  new ActMain(),
+     *          '/login' =>  new ActLogin(),
+     *          '/logout' => new ActLogout()
+     *     ]
+     */
+    public function __construct($path, array $actionMap = [])
+    {
+        if (is_string($path)) {
+            $this->request = new RqSERVER([ 'REQUEST_URI' => $path ]);
+        } elseif ($path instanceof Request) {
+            $this->request = $path;
+        } else {
+            throw new Exception("\$path must be string or Request-object");
+        }
+
+        
+        $this->actionMap = $actionMap;
+    }
+
+    public function handle(Response $resp): Response
+    {
+        $requestUri = $this->request->param('REQUEST_URI');
+
+        if (! array_key_exists($requestUri, $this->actionMap)) {
+            throw new NotFoundException('Link=' . $requestUri . ' is not found in actions list!');
+        }
+
+        return $this->actionMap[$requestUri]->handle($resp);
+    }
+}
