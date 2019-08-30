9b890c470a67b32bab7b752600c321c35dc5b7cb
klepatskiy <nklepatskiy@gmail.com>
2019-08-30 11:50:30 +0300

    php doc

--- src/Response/RespJson.php
+++ src/Response/RespJson.php
@@ -3,11 +3,26 @@ namespace Lipid\Response;
 
 use Lipid\Response;
 
+/**
+ * Response json (Decorator RespStd)
+ *
+ * Example (in action):
+ * ```php
+ *   return new RespJson(['result' => 'ok', 'message' => $res], $resp);
+ * ```
+ * @author
+ */
 final class RespJson implements Response
 {
     private $json;
     private $orig;
 
+    /**
+     * RespJson constructor.
+     *
+     * @param mixed $json any data to return as json
+     * @param Response|null $orig
+     */
     public function __construct($json, Response $orig = null)
     {
         $this->json = $json;
@@ -27,7 +42,7 @@ final class RespJson implements Response
     public function print(): void
     {
         $this->withBody(json_encode($this->json, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE))
-            ->withHeaders(['Content-type' => 'json'])
+            ->withHeaders(['Content-type' => 'application/json'])
             ->print();
     }
 }
