d317e27d3ca0e173867559cf55b68a4910f24d2f
klepa <nklepatskiy@gmail.com>
2021-09-21 17:45:26 +0300

    Обновил pdo

--- src/BasePDO.php
+++ src/BasePDO.php
@@ -3,6 +3,7 @@
 namespace Lipid;
 
 use PDO;
+use PDOStatement;
 
 /**
  * BasePDO - decorator with lazy load
@@ -18,8 +19,8 @@ use PDO;
  */
 class BasePDO extends PDO
 {
-    
-    private $config;
+
+    private Config $config;
 
     public function __construct(Config $config)
     {
@@ -56,57 +57,57 @@ class BasePDO extends PDO
         return $this->pdo()->commit();
     }
 
-    public function errorCode()
+    public function errorCode(): ?string
     {
         return $this->pdo()->errorCode();
     }
 
-    public function errorInfo() : array
+    public function errorInfo(): array
     {
         return $this->pdo()->errorInfo();
     }
 
-    public function exec($query)
+    public function exec($query): int|false
     {
         return $this->pdo()->exec($query);
     }
 
-    public function getAttribute($attribute)
+    public function getAttribute(int $attribute): mixed
     {
         return $this->pdo()->getAttribute($attribute);
     }
 
-    public function inTransaction() : bool
+    public function inTransaction(): bool
     {
         return $this->pdo()->inTransaction();
     }
 
-    public function lastInsertId($seqname = null)
+    public function lastInsertId(?string $seqname = null): string|false
     {
         return $this->pdo()->lastInsertId($seqname);
     }
 
-    public function prepare($statement, $driverOptions = null)
+    public function prepare(string $statement, array $driverOptions = []): PDOStatement|false
     {
         return $this->pdo()->prepare($statement, $driverOptions);
     }
 
-    public function query(string $statement, int $fetchMode = null, mixed ...$fetchModeArgs)
+    public function query(string $statement, ?int $fetchMode = null, mixed ...$fetchModeArgs): PDOStatement|false
     {
         return $this->pdo()->query($statement, $fetchMode, ...$fetchModeArgs);
     }
 
-    public function quote($string, $paramtype = null)
+    public function quote(string $string, int $paramtype = PDO::PARAM_STR): string|false
     {
         return $this->pdo()->quote($string, $paramtype);
     }
 
-    public function rollBack() : bool
+    public function rollBack(): bool
     {
         return $this->pdo()->rollBack();
     }
 
-    public function setAttribute($attribute, $value) : bool
+    public function setAttribute($attribute, $value): bool
     {
         return $this->pdo()->setAttribute($attribute, $value);
     }
